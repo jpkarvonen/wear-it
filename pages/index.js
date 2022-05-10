9106e7b2371cc0cbca0f345aad1156fd6d1fe6e3
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2022-05-10 15:34:03 -0500

    Commit

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
+
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
